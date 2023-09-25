1da7d760d9bb457b7b016a56fff429245dcce4a8
mobasshirCode <mobasshir.code@gmail.com>
2023-09-25 14:16:32 +0530

    Minor fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import './Navbar.scss'
+import React from 'react';
+import './Navbar.scss';
+import { Link } from 'react-router-dom';
+
 
 export default function Navbar() {
   return (
@@ -10,24 +12,24 @@ export default function Navbar() {
       </div>
       <div className="second">
         <ul>
-            <li><a href="/">HOME</a></li>
-            <li><a href="/top">TOP ANIME</a></li>
-            <li><a href="/upcoming">UPCOMING</a></li>
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="/top">TOP ANIME</Link></li>
+            <li><Link to="/upcoming">UPCOMING</Link></li>
             <div className="dropdown">
     <button className="dropbtn">RATING
       <i className="fa fa-caret-down"></i>
     </button>
     <div className="dropdown-content">
-      <a href="/all">FAMILY</a>
-      <a href="/child">CHILDREN</a>
-      <a href="/teen">TEENS (13+)</a>
-      <a href="/17">VIOLANCE (17+)</a>
-      <a href="/18">MILD NUDITY</a>
-      <a href="/18+">HENTAI</a>
+      <Link to="/all">FAMILY</Link>
+      <Link to="/child">CHILDREN</Link>
+      <Link to="/teen">TEENS (13+)</Link>
+      <Link to="/17">VIOLANCE (17+)</Link>
+      <Link to="/18">MILD NUDITY</Link>
+      <Link to="/18+">HENTAI</Link>
     </div>
   </div>
-            <li><a href="/manga">MANGA</a></li>
-            <li><a href="/about">ABOUT</a></li>
+            <li><Link to="/manga">MANGA</Link></li>
+            <li><Link to="/about">ABOUT</Link></li>
         </ul></div>
     </nav>
   </>
