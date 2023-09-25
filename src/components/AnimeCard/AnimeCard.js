fb42f9838ccf939403269ad27c921090eda3d37e
mobasshirCode <mobasshir.code@gmail.com>
2023-09-25 06:15:10 +0530

    Added ratings

--- src/components/AnimeCard/AnimeCard.js
+++ src/components/AnimeCard/AnimeCard.js
@@ -4,7 +4,8 @@ import './AnimeCard.scss'
 export default function AnimeCard(props) {
   return (
     <div className='card'>
-      <img src={props.imgurl} alt="image" />
+      <img src={props.imgurl} alt="anime" />
+      <h6>&#11088;{props.score}</h6>
       <h3>{props.title}</h3>
     </div>
   )
--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -5,31 +5,43 @@ import AnimeCard from '../AnimeCard/AnimeCard'
 export default function Popular() {
 
 const [details, setDetails] = useState([])
+const [page, setPage] = useState(1)
 
   const topAnime = async()=> {
-    let url = "https://api.jikan.moe/v4/top/anime?filter=bypopularity"
+    let url = `https://api.jikan.moe/v4/top/anime?filter=bypopularity&limit=24&page=${page}`
     let data = await fetch(url)
     let parsedData = await data.json()
-    console.log(parsedData.data)
+    // console.log(parsedData.data)
     setDetails(parsedData.data)
   }
   
   useEffect(() => {
       topAnime()
+      // eslint-disable-next-line
   }, [])
+
+  const loadMore = async()=> {
+    let url = `https://api.jikan.moe/v4/top/anime?filter=bypopularity&page=${page+1}`
+    let data = await fetch(url)
+    let parsedData = await data.json()
+ setPage(page+1)
+setDetails(details.concat(parsedData.data))
+
+  }
 
   return (
     <>
     <h2 className='head'>Top Anime</h2>
     <div className="innerContainer">
     {details.map((element)=> {
-    return (
-      <AnimeCard imgurl={element.images.jpg.image_url} title={element.title_english}/>
-    )
+    return (<>
+      <AnimeCard key={element.mal_id} imgurl={element.images.jpg.image_url} title={element.title_english} score={element.score}/>
+    </>)
     })}
-    
     </div>
-  
+    <div className='btnc'>
+    <button className='btnMore' onClick={loadMore}>Show more</button>
+    </div>
     </>
   )
 }
